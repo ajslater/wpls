4e8776f81926225fc80477e6bfac40424af7c991
AJ Slater <aj@slater.net>
2026-03-05 01:08:08 -0800

    dummy tests

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Wallpaper Tool Tests."""
---
+++ tests/test_dummy.py
@@ -0,0 +1,9 @@
+"""Dummy Test."""
+
+
+class TestDummy:
+    """Dummy test."""
+
+    def test_dummy(self) -> None:
+        """Noop."""
+        assert True
